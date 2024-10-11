a6bed6bb4dfc08b2f811540564d5060c38fbc6cf
BUMJUNOH <59131152+ohbumjun@users.noreply.github.com>
2024-10-11 16:45:29 +0900

    Title: 루돌프의 반란, Time: 12ms, Memory: 0MB, Status: Passed - CodeTree

--- 241011/루돌프의 반란/rudolph-rebellion.cpp
+++ 241011/루돌프의 반란/rudolph-rebellion.cpp
@@ -23,26 +23,24 @@
 using namespace std;
 
 int N, M, P, C, D;
-int ans = 0;
 
-// 8 방향, 상우하좌, 대각선 (왼위, 오위, 왼아래, 오아래)
-int dRow[] = {-1, 0, 1, 0, -1, -1, 1, 1};
-int dCol[] = {0,  1, 0, -1, -1, 1, -1, 1};
+// 점수
+vector<int> scores;
+// 탈락 여부
+vector<bool> alive;
+// 기절 타임 (turn 수로 지정)
+vector<int> stun;
+// 위치
+vector<pair<int, int>> poses;
+// 루돌프 위치
+int roudRow, roudCol;
+// 위치 2차원 벡터
+vector<vector<int>> board;
+// 상우하좌
+int dRow[] = { -1, 0, 1, 0 };
+int dCol[] = { 0, 1, 0, -1 };
 
-vector<vector<int>> board;	// N * N
-vector<bool> aliveSantas;	// 탈락 여부
-vector<int> deadTime;		// 기절 시간 -> 0보다 크면, 기절 상태
-int roudolfRow, roudolfCol;
-vector<vector<int>> stataPosMap;	// N * N
-vector<int> santaScores;
-
-// 1. 어려웠던 것 : 상우좌하순
-// 루돌프가 가장 가까운 산타 방향으로 돌진..?
-// 2. 루돌프가 충돌하는 직후에 , 바로 산타가 날아가는 것.
-// 산타가 모두 기다린 이후에 처리하는 것 아니다.
-// 3. 하나의 턴에서 루돌프와 산타 충돌이 동시에 발생할 수 있다.
-
-int optDir(int dir)
+int getOptDir(int dir)
 {
 	if (dir == 0)
 		return 2;
@@ -53,271 +51,297 @@ int optDir(int dir)
 	else if (dir == 3)
 		return 1;
 }
+
 void Input()
 {
 	cin >> N >> M >> P >> C >> D;
 
-	board.resize(N, vector<int>(N, 0));
-	stataPosMap.resize(N, vector<int>(N, -1));
+	cin >> roudRow >> roudCol;
+	roudRow -= 1;
+	roudCol -= 1;
 
-	cin >> roudolfRow >> roudolfCol;
-	roudolfRow -= 1;
-	roudolfCol -= 1;
+	// - 1은 아무것도 X
+	// 0 이상부터 산타
+	board.resize(N, vector<int>(N, -1));
 
-	santaScores.resize(P + 1, 0);
-	deadTime.resize(P + 1, 0);
-	aliveSantas.resize(P + 1, true);
+	scores.resize(P, 0);
+	alive.resize(P, true);
+	stun.resize(P, 0);
+	poses.resize(P);
 
-	for (int p = 1; p <= P; ++p)
+	for (int p = 0; p < P; ++p)
 	{
-		int pIdx, r, c;
-		cin >> pIdx >> r >> c;
-		stataPosMap[r - 1][c - 1] = pIdx;
+		int n, r, c;
+		cin >> n >> r >> c;
+		n -= 1;
+		r -= 1;
+		c -= 1;
+		poses[n] = { r, c };
+		board[r][c] = n;
 	}
 }
 
-// move_santa 도 queue 형태로 처리해도 된다.
-void moveSanta(int stR, int stC, int moveCnt, 
-	int santa, int addedR, int addedC)
+void moveSanta(int santa, int row, int col, int rowDir, int colDir, int moveCnt)
 {
-	// int initRow = stR + dRow[dir];
-	// int initCol = stC + dCol[dir];
-
-	int initRow = stR + addedR * moveCnt;
-	int initCol = stC + addedC * moveCnt;
-
-	stataPosMap[stR][stC] = -1;
-
-	// 만약 범위 벗어나면 그냥 out 처리
-	if (initRow < 0 || initRow >= N || initCol < 0 || initCol >= N)
-	{
-		aliveSantas[santa] = false;
-		return;
-	}
-
-	queue<tuple<int,int, int>> q;
-	q.push({initRow, initCol, santa });
+	//row,col 에 있는 santa 를 rowDir, colDir 방향으로 moveCnt 만큼 이동시킴
+	queue<tuple<int, int, int, int>> q;
+	q.push({ santa, row, col, moveCnt });
 
 	while (!q.empty())
 	{
-		// q. 에 있는 녀석을
-		int curRow, curCol, curSanta;
-		tie(curRow, curCol, curSanta) = q.front();
+		int santa, curRow, curCol, curMoveCnt;
+		tie(santa, curRow, curCol, curMoveCnt) = q.front();
 		q.pop();
 
-		// 범위를 벗어났다면, 탈락 처리
-		if (curRow < 0 || curRow >= N || curCol < 0 || curCol >= N)
+		int nxtRow = curRow + rowDir * curMoveCnt;
+		int nxtCol = curCol + colDir * curMoveCnt;
+
+		// 범위를 벗어난다면
+		if (nxtRow < 0 || nxtRow >= N || nxtCol < 0 || nxtCol >= N)
 		{
-			aliveSantas[curSanta] = false;
-			continue;
+			// 해당 산타 탈락 처리하고
+			// 바로 return
+			alive[santa] = false;
+			return;
 		}
 		else
 		{
-			// 범위 안
-			if (stataPosMap[curRow][curCol] == -1)
+			// 범위를 벗어나지 않는다면
+			// 1) 해당 위치에 산타가 있다면
+			if (board[nxtRow][nxtCol] != -1)
 			{
-				stataPosMap[curRow][curCol] = curSanta;
-				// 2) 해당 위치에 기존 산타가 없었다면, 
-				//	이동 처리만 하고 바로 나온다.
+				int oldSanta = board[nxtRow][nxtCol];
+				q.push({oldSanta, nxtRow, nxtCol, 1});
+
+				// 새로운 산타로 세팅
+				board[nxtRow][nxtCol] = santa;
+				poses[santa] = { nxtRow, nxtCol };
 			}
 			else
 			{
-				// 1) 해당 위치에 기존 산타가 있었다면.
-				//	queue 에 다시 넣는다.
-				int oldSanta = stataPosMap[curRow][curCol];
-				stataPosMap[curRow][curCol] = curSanta;
-				q.push({ curRow + addedR,
-					curCol + addedC, oldSanta });
+				// 2) 산타가 없다면
+				// 새로운 위치로 이동 처리하고 return
+				board[nxtRow][nxtCol] = santa;
+				poses[santa] = { nxtRow, nxtCol };
+				return;
 			}
 		}
+
 	}
 }
 
 void Solve()
 {
+	/*
+	1) 게임판 구성
+	총 M 번의 턴
+	- 매 턴마다 루돌프, 산타 움직임
+	- 루돌프 1번. 이후 산타 1 ~ P 차례대로 이동
+	  - 기절 혹은 탈락 산타는 이동 X
+	- 거리 계산 : (r - r)^2 + (c - c)^2
+	*/
 	for (int turn = 0; turn < M; ++turn)
 	{
-		// 모든 산타 탈락이면 break.
-		int aliveCnt = 0;
-		for (int p = 1; p <= P; ++p)
-			if (aliveSantas[p])
-				aliveCnt += 1;
-
-		// 1. 루돌프 이동
-		// - 모든 산타 비교 => bfs (X), 그냥 산타 위치랑 일일히 비교
-		// - 행큰, 열큰 정렬 (sort 함수 사용)
-		// - 루돌프 방향도, bfs 가 아니라, 그냥 row, col 끼리만 비교.
-		int rouDirR = 0, rouDirC = 0;
+		/*
+		2) 루돌프 움직임
+		- 가장 가까운 산타 1칸 돌진
+		- 대신, 탈락하지 않은 산타여야 한다.
+		- 가까운 산타 2명 이상 (행큰, 열큰)
+		- 8방향 이동 가능
+		4) 루돌프에 의한 충돌
+		  - 산타 + C 점수
+		  - 산타는 루돌프가 이동해온 방향 C 칸 이동
+		  - 밀려난 위치 게임 밖 -> 탈락
+		  - 밀려난 칸에 다른 산타 ? 상호작용
+		  - 산타 기절, 2 턴 이후부터 이동 가능
+		*/
 		int minDist = INT_MAX;
-		vector<pair<int,int>> posToSantas;
+		vector<pair<int,int>> minSantaInfos;
 
-		for (int r = 0; r < N; ++r)
+		for (int p = 0; p < P; ++p)
 		{
-			for (int c = 0; c < N; ++c)
+			if (alive[p] == false)
+				continue;
+			
+			//if (stun[p] > turn) 기절도 충돌 대상
+			//	continue;
+
+			int santaRow	= poses[p].first;
+			int santaCol	= poses[p].second;
+			int curDist		= (roudRow - santaRow) * (roudRow - santaRow) + 
+				(roudCol - santaCol) * (roudCol - santaCol);
+
+			if (minDist > curDist)
 			{
-				if (stataPosMap[r][c] == -1)
-					continue;
-				int dist = (r - roudolfRow) * (r - roudolfRow) 
-					+ (c - roudolfCol) * (c - roudolfCol);
-				if (dist < minDist)
-				{
-					minDist = dist;
-					posToSantas.clear();
-					posToSantas.push_back({ r, c });
-				}
-				else if (dist == minDist)
-				{
-					posToSantas.push_back({ r, c });
-				}
+				minDist = curDist;
+				minSantaInfos.clear();
+				minSantaInfos.push_back({ santaRow , santaCol});
+			}
+			else if (minDist == curDist)
+			{
+				minSantaInfos.push_back({ santaRow , santaCol});
 			}
 		}
+		
+		// 행큰, 얼큰
+		sort(minSantaInfos.begin(), minSantaInfos.end());
 
-		int closeStRow = posToSantas[posToSantas.size() - 1].first;
-		int closeStCol = posToSantas[posToSantas.size() - 1].second;
+		const pair<int,int>& minSantaPos = minSantaInfos[minSantaInfos.size() - 1];
+		int minSantaRow = minSantaPos.first;
+		int minSantaCol = minSantaPos.second;
+		int minSanta = board[minSantaRow][minSantaCol];
 
-		if (closeStRow > roudolfRow)
-			rouDirR = 1;
-		else if (closeStRow < roudolfRow)
-			rouDirR = -1;
+		int rouMovR = 0, rouMovC = 0;
 
-		if (closeStCol > roudolfCol)
-			rouDirC = 1;
-		else if (closeStCol < roudolfCol)
-			rouDirC = -1;
+		if (roudRow < minSantaRow)
+			rouMovR = 1;
+		else if (roudRow > minSantaRow)
+			rouMovR = -1;
 
-		roudolfRow += rouDirR;
-		roudolfCol += rouDirC;
+		if (roudCol < minSantaCol)
+			rouMovC = 1;
+		else if (roudCol > minSantaCol)
+			rouMovC = -1;
 
-		if (stataPosMap[roudolfRow][roudolfCol] != -1)
+		roudRow += rouMovR;
+		roudCol += rouMovC;
+
+		// 루돌프에 의한 충돌
+		if (board[roudRow][roudCol] != -1)
 		{
-			// - 충돌시 : move_santa(상호작용) => 이거를 나는 마지막에 했었음.
-			//		- 점수 획득
-			//		- 기절 처리.
-			int collideSanta = stataPosMap[roudolfRow][roudolfCol];
+			int collideSanta = board[roudRow][roudCol];
 
-			santaScores[collideSanta] += C;
-			deadTime[collideSanta] += 2;
+			// 점수
+			scores[collideSanta] += C;
 
-			moveSanta(roudolfRow, roudolfCol, C, 
-				collideSanta, rouDirR, rouDirC);
+			// 기절 처리
+			stun[collideSanta] = turn + 2;
+
+			// 산타 기존 위치 update
+			board[roudRow][roudCol] = -1;
+
+			// 안에서 새로운 산타 위치 정보 세팅해줄 것이다.
+			moveSanta(collideSanta, 
+				roudRow, roudCol, rouMovR, rouMovC, C);
+		}
+		else
+		{
+			// 별다른 조치 X
 		}
 
-
-		// 1. move santa
-		// 2. dead time 처리 방식.
-
-
-		// 2. 모든 산타 이동
-		// - 기절, 탈락 제외
-		// - 루돌프 가까워지는 방향
-		//		- 다른 산타 X
-		//		- 상,우,좌,하 순으로 (우선 순위 고려하는 방법 간단하다)
-		//		- 경로는 어떻게 얻어옴 ? => 나는 BFS 에서 아예 배열로 함.
-		//		- 마찬가지로 그냥 모든 경로 고려할 필요 X. 
-		//			그냥 4방향 탐색 only
-
-		for (int p = 1; p <= P; ++p)
+		/*
+		3) 산타 움직임
+		- 1 ~ P 번까지 "순서대로" 이동
+		- 기절 or 탈락 X
+		- 루돌프에게 가까워지는 방향으로 1칸
+		- 다른 산타 있는 칸. 혹은 게임밖 X
+		- 움직일 수 있는 칸이 있더라도, 루돌프와 더 가까워지지 않으면 X
+		- 상우하좌 우선순위 이동
+		 5) 산타에 의한 충돌
+		  - D 점 획득
+		  - 이동 방향 반대로 D 칸 이동
+		  - 마찬가지로 밀려난 위치 게임 밖 ? -> 탈락
+		  - 밀려난 칸에 다른 산타 ? 상호작용
+		  - 산타 기절, 2 턴 이후부터 이동 가능
+		*/
+		for (int p = 0; p < P; ++p)
 		{
-			if (aliveSantas[p] == false)
+			if (alive[p] == false)
 				continue;
-			if (deadTime[p] > 0)
-			{
-				deadTime[p] -= 1;
+			if (stun[p] > turn)
 				continue;
-			}
-
-			// board 를 뒤져서, 산타 위치를 찾는다
-			int stRow, stCol;
-			for (int r = 0; r < N; ++r)
-			{
-				for (int c = 0; c < N; ++c)
-				{
-					if (stataPosMap[r][c] == p)
-					{
-						stRow = r;
-						stCol = c;
-						break;
-					}
-				}
-			}
-
-			int minDist = (stRow - roudolfRow) *
-				(stRow - roudolfRow)
-				+ (stCol - roudolfCol) * 
-				(stCol - roudolfCol);
-
-			vector<int> santaNextDir;
-
+			int stRow = poses[p].first;
+			int stCol = poses[p].second;
+			int distToRou = (stRow - roudRow) * (stRow - roudRow)
+				+ (stCol - roudCol) * (stCol - roudCol);
+			int moveDir = -1;
 			for (int d = 0; d < 4; ++d)
 			{
 				int nxtRow = stRow + dRow[d];
 				int nxtCol = stCol + dCol[d];
-				// 범위 out 고려 X
+				// 범위 밖
 				if (nxtRow < 0 || nxtRow >= N || nxtCol < 0 || nxtCol >= N)
 					continue;
 				// 다른 산타 X
-				if (stataPosMap[nxtRow][nxtCol] != -1)
+				int info = board[nxtRow][nxtCol];
+				assert(info != p);
+				if (info != -1)
 					continue;
-				int curDist = (nxtRow - roudolfRow) * 
-					(nxtRow - roudolfRow) 
-					+ (nxtCol - roudolfCol) * 
-					(nxtCol - roudolfCol);
-				if (curDist < minDist)
+				int nxtDist = (nxtRow - roudRow) * (nxtRow - roudRow)
+					+ (nxtCol - roudCol) * (nxtCol - roudCol);
+				if (distToRou > nxtDist)
 				{
-					minDist = curDist;
-					santaNextDir.push_back(d);
+					distToRou = nxtDist;
+					moveDir = d;
 				}
 			}
-
-			if (santaNextDir.size() == 0)
+			if (moveDir == -1)
 				continue;
-
-			// 맨 마지막
-			int minDir = santaNextDir[santaNextDir.size() - 1];
-
-			// 이동
-			int movedRow = stRow + dRow[minDir];
-			int movedCol = stCol + dCol[minDir];
-
-			stataPosMap[stRow][stCol] = -1;
-
-			// - 루돌프 충돌시 
-			//	- 점수
-			//	- 기절 처리
-			//	- 상호 작용
-			if (movedRow == roudolfRow && movedCol == roudolfCol)
+			// 이동 처리
+			int moveRow = stRow + dRow[moveDir];
+			int moveCol = stCol + dCol[moveDir];
+			// 기존 위치 X
+			board[stRow][stCol] = -1;
+			// 루돌프 충돌 여부 확인하기
+			if (moveRow == roudRow && moveCol == roudCol)
 			{
-				santaScores[p] += D;
-				deadTime[p] += 2;
-				// 반대 방향
-				int moveDir = optDir(minDir);
-				int addedR = dRow[moveDir];
-				int addedC = dCol[moveDir];
-				moveSanta(movedRow, movedCol, D, p, 
-					addedR, addedC);
+				// -D 점 획득
+				scores[p] += D;
+
+				// - 산타 기절, 2 턴 이후부터 이동 가능
+				stun[p] = turn + 2;
+
+				moveDir = getOptDir(moveDir);
+
+				// 산타 이동 처리 (moveSanta 내부에서 사용)
+				// board[moveRow][moveCol] = p;
+
+				// - 이동 방향 "반대로" D 칸 이동
+				// - 마찬가지로 밀려난 위치 게임 밖 ? ->탈락
+				// - 밀려난 칸에 다른 산타 ? 상호작용
+				moveSanta(p, roudRow, roudCol, dRow[moveDir], dCol[moveDir], D);
 			}
 			else
 			{
-				// 충돌 X
-				stataPosMap[movedRow][movedCol] = p;
+				// 위치 정보 update
+				board[moveRow][moveCol] = p;
+				poses[p] = { moveRow, moveCol };
 			}
 		}
 
-		// 3. alive 인 경우 점수 + 1
-		for (int p = 1; p <= P; ++p)
+		/*
+		7) 게임 종료
+		P 명 산타 모두 탈락시 게임 종료
+		매턴 마자, 아직 탈락 X 산타는 점수 + 1
+		*/
+		bool allDead = true;
+		for (int p = 0; p < P; ++p)
 		{
-			if (aliveSantas[p] == false)
+			if (alive[p])
+			{
+				allDead = false;
+				break;
+			}
+		}
+
+		if (allDead)
+			break;
+
+		for (int p = 0; p < P; ++p)
+		{
+			if (alive[p] == false)
 				continue;
-			santaScores[p] += 1;
+			scores[p] += 1;
 		}
 	}
 
-	for (int p = 1; p <= P; ++p)
+	// 각 산타가 얻은 최종 점수 출력
+	for (int p = 0; p < P; ++p)
 	{
-		cout << santaScores[p] << " ";
+		cout << scores[p] << " ";
 	}
+	
 }
-
 
 int main() {
 	ios::sync_with_stdio(false);
